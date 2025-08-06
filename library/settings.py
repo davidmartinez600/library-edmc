8854832754b4a57ce962de66ef672344552f6c9c
davidmartinez600 <esdavidmartinez600@gmail.com>
2025-08-06 13:19:24 -0500

    Edit settings

--- library/settings.py
+++ library/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zrokc=j1y8s!b%)uj+z5crp91^zap)y)n@(doycgn$fut!19uz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['library-test-edmc.vercel.app']
 
 
 # Application definition
